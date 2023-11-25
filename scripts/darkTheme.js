ceb02b75256e1053b2dfb1ab2c053fdd051c1448
Artemis <arsitur4@gmail.com>
2023-11-25 17:54:28 +0200

    Finished darkmode everywhere, added reviews

---
+++ scripts/darkTheme.js
@@ -0,0 +1,22 @@
+// Dark theme
+
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+const isLightMode = localStorage.getItem('lightMode') === 'true';
+
+if (isDarkMode)
+{
+    document.body.classList.add('dark-theme');
+    document.documentElement.style.colorScheme = 'dark';
+}
+if (!isLightMode && !isDarkMode)
+{
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    {
+        document.body.classList.add('dark-theme');
+        document.documentElement.style.colorScheme = 'dark';
+    } else
+    {
+        document.body.classList.remove('dark-theme');
+        document.documentElement.style.colorScheme = 'light';
+    }
+}
